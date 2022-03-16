1684ac0e961f9eeb1214997cf5188431aec628e5
BlueZeeKing <52867955+BlueZeeKing@users.noreply.github.com>
2022-03-16 16:18:05 -0400

    added background image

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,24 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Blueish</title>
-        <meta name="description" content="Blueish.dev home page" />
+        <title>Fjordboard</title>
+        <meta name="description" content="fjordboard.com home page" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
 
-      <main className="bg-black h-screen flex flex-col">
+      <main className="bg-[#F0BA36] h-screen flex flex-col bg-center bg-contain bg-no-repeat bg-[url('/background.png')]">
         <div className="flex-grow"></div>
-        <h1 className="text-gray-200 text-center font-extrabold text-5xl">fjordboard.com is currently a WIP</h1>
+        <h1 className="text-gray-200 text-center font-extrabold text-5xl">
+          fjordboard.com is currently a WIP
+        </h1>
         <div className="flex-grow"></div>
       </main>
 
       <footer>
-        <h3 className="text-gray-400 text-center absolute w-screen bottom-0 underline p-6"><a href="mailto:frugivore@fjordboard.com">Frugivore</a></h3>
+        <h3 className="text-slate-200 text-center absolute w-screen bottom-0 underline p-6">
+          <a href="mailto:frugivore@fjordboard.com">Contact</a>
+        </h3>
       </footer>
     </div>
-  )
+  );
 }
